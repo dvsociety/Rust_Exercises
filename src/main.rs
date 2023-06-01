617152f0f267935b317b00fc0a7151647298acca
Pablo Martin Morenos <pablomartinmoreno@protonmail.com>
2023-06-01 10:09:36 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,2 @@
 fn main() {
-    let a: u32= 10;
-    let b: u32= 7;
-    println!("{}", euclides(a, b));
-}
-
-fn euclides(a: u32, b: u32) -> u32 {
-    if b == 0 {
-        a
-    } else {
-        let r = a % b; 
-        euclides(b, r)
-    }
 }
