2c082dd796dd53c614830ab7e91ea46240a1995a
Pablo Martin Morenos <pablomartinmoreno@protonmail.com>
2023-06-03 13:10:38 -0300

    .

--- src/3.4_euclides.rs
+++ src/3.4_euclides.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("{}",euclides(543, 240));
+    println!("{}",euclides_v2(543, 240));
 }
 
 fn euclides(m: u32, n: u32) -> u32 {
@@ -10,4 +11,13 @@ fn euclides(m: u32, n: u32) -> u32 {
         let r = m % n;
         euclides(n, r)
     }
+}
+
+fn euclides_v2(mut a:u32, mut b:u32) -> u32 {
+    while b != 0 {
+        let r:u32 = a % b; 
+        a = b; 
+        b = r;
+    }
+    a
 }
