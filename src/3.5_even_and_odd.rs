4caa3d2cc94ec2fb3e245d9bae492e38d1a7d932
dvsociety <pablomartinmoreno@protonmail.com>
2023-06-29 11:10:43 -0300

    .

--- src/3.5_even_and_odd.rs
+++ src/3.5_even_and_odd.rs
@@ -1,7 +1,18 @@
+// If the number is even, it should be returned in binary; otherwise, in hexadecimal.
+
 fn main() {
-    is_even(30);
+    let number = 31;
+    println!("{}, {}", is_even(number), even_and_odd(number));
 }
 
-fn is_even(number: i32) -> bool {
-    number % 2 == 0
+fn is_even(n: i32) -> bool {
+    n % 2 == 0
+}
+
+fn even_and_odd(n: i32) -> String {
+    if n % 2 == 0 {
+        format!("{:b}", n)
+    } else {
+        format!("{:x}", n)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+/*Write a function that takes in a string of one or more words, 
+and returns the same string, but with all five or more letter 
+words reversed (Just like the name of this Kata). 
+Strings passed in will consist of only letters and spaces. 
+Spaces will be included only when more than one word is present.*/
 
+fn main() {
+    let message: &str = "hola mundo";
+    let first_word: &str = get_fist_world(&message);
+    println!("{first_word}");
+}
+
+fn get_fist_world(s: &str) -> &str {
+
+    let bytes = s.as_bytes();
+
+    for (index, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..index];
+        }
+    }
+    &s
 }
